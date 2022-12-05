bfd8c689ddc6a5fe335772540978627f06422e24
Kseniia Vilgodskaia <vilgodskaya.ksenia@gmail.com>
2022-12-05 17:09:55 +0200

    Initialize project

---
+++ src/main/java/com/example/movieplatformpetproject/MoviePlatformPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieplatformpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePlatformPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviePlatformPetProjectApplication.class, args);
+    }
+
+}
