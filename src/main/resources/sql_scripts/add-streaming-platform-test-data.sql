7f61f1e7ecd8531fd29af2d47881a0c8a1a9db18
Kseniia Vilgodskaia <vilgodskaya.ksenia@gmail.com>
2022-12-27 18:37:07 +0200

    - Add sql scripts for streaming platform table in DB

---
+++ src/main/resources/sql_scripts/add-streaming-platform-test-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO streaming_platform(id, name)
+VALUES ('385057e2-8cd0-456c-8c97-4f8006b49af9', 'Netflix'),
+       ('520b9c3f-e569-4930-914d-7fd1c88317d9', 'Hulu'),
+       ('d61147e8-d6b0-4c72-b0ae-402d723bb362', 'HBO'),
+       ('09a43f8c-e28b-4900-bb2b-b415915aeec5', 'Disney'),
+       ('5a55836d-dc80-45b4-8d38-84ce39eaaef9', 'Apple TV')
---
+++ src/main/resources/sql_scripts/create-streaming-platform-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE streaming_platform
+(
+    id   uuid         NOT NULL PRIMARY KEY,
+    name varchar(254) NOT NULL UNIQUE
+)
