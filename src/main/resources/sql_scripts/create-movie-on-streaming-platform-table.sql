0d541a50a0ab055ab78245447cea173f9ed0880f
Kseniia Vilgodskaia <vilgodskaya.ksenia@gmail.com>
2022-12-30 16:27:36 +0200

    - Change FK names in constraints of movie_on_streaming_platform table - Add check for price_for_buying field in movie_on_streaming_platform table

--- src/main/resources/sql_scripts/create-movie-on-streaming-platform-table.sql
+++ src/main/resources/sql_scripts/create-movie-on-streaming-platform-table.sql
@@ -5,10 +5,10 @@ CREATE TABLE movie_on_streaming_platform
     streaming_platform_id     uuid    NOT NULL,
     available_for_buying      boolean NOT NULL,
     available_in_subscription boolean NOT NULL,
-    price_for_buying          integer,
+    price_for_buying          integer CHECK ( price_for_buying IS NULL OR price_for_buying >= 0),
     available_until           date,
     UNIQUE (movie_id, streaming_platform_id),
     CHECK ( available_for_buying OR available_in_subscription ),
-    CONSTRAINT fk_movie FOREIGN KEY (movie_id) REFERENCES movie (id),
-    CONSTRAINT fk_streaming_platform FOREIGN KEY (streaming_platform_id) REFERENCES streaming_platform (id)
+    CONSTRAINT FK_movie_on_streaming_platform_movie FOREIGN KEY (movie_id) REFERENCES movie (id),
+    CONSTRAINT FK_movie_on_streaming_platform_streaming_platform FOREIGN KEY (streaming_platform_id) REFERENCES streaming_platform (id)
 )
