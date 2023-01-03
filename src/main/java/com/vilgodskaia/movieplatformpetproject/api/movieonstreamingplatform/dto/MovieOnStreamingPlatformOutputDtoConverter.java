758ae08614440723d03f6f00de20af65893d371c
Kseniia Vilgodskaia <vilgodskaya.ksenia@gmail.com>
2023-01-03 12:26:39 +0200

    Change from Movie to MovieOutputDto and from StreamingPlatform to StreamingPlatformOutputDto in MovieOnStreamingPlatformOutputDto

--- src/main/java/com/vilgodskaia/movieplatformpetproject/api/movieonstreamingplatform/dto/MovieOnStreamingPlatformOutputDtoConverter.java
+++ src/main/java/com/vilgodskaia/movieplatformpetproject/api/movieonstreamingplatform/dto/MovieOnStreamingPlatformOutputDtoConverter.java
@@ -1,16 +1,23 @@
 package com.vilgodskaia.movieplatformpetproject.api.movieonstreamingplatform.dto;
 
+import com.vilgodskaia.movieplatformpetproject.api.movie.dto.MovieOutputDtoConverter;
+import com.vilgodskaia.movieplatformpetproject.api.streamingplatform.dto.StreamingPlatformOutputConverter;
 import com.vilgodskaia.movieplatformpetproject.model.MovieOnStreamingPlatform;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MovieOnStreamingPlatformOutputDtoConverter {
+
+    private final MovieOutputDtoConverter movieOutputDtoConverter;
+    private final StreamingPlatformOutputConverter streamingPlatformOutputConverter;
 
     public MovieOnStreamingPlatformOutputDto convert(MovieOnStreamingPlatform movieOnStreamingPlatform) {
         return new MovieOnStreamingPlatformOutputDto()
                 .setId(movieOnStreamingPlatform.getId())
-                .setMovie(movieOnStreamingPlatform.getMovie())
-                .setStreamingPlatform(movieOnStreamingPlatform.getStreamingPlatform())
+                .setMovieOutputDto(movieOutputDtoConverter.convert(movieOnStreamingPlatform.getMovie()))
+                .setStreamingPlatformOutputDto(streamingPlatformOutputConverter.convert(movieOnStreamingPlatform.getStreamingPlatform()))
                 .setAvailableForBuying(movieOnStreamingPlatform.isAvailableForBuying())
                 .setAvailableInSubscription(movieOnStreamingPlatform.isAvailableInSubscription())
                 .setPriceForBuying(movieOnStreamingPlatform.getPriceForBuying())
