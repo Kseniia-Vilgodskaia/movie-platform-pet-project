7615385bdbfc9f8c57f05294616aae844530c2bc
Kseniia Vilgodskaia <vilgodskaya.ksenia@gmail.com>
2023-01-03 12:54:47 +0200

    Shorten the inputDtos names for MovieOnStreamingPlatform

--- src/main/java/com/vilgodskaia/movieplatformpetproject/api/movieonstreamingplatform/dto/MovieOnStreamingPlatformCreateDto.java
+++ src/main/java/com/vilgodskaia/movieplatformpetproject/api/movieonstreamingplatform/dto/MovieOnStreamingPlatformCreateDto.java
@@ -3,10 +3,13 @@ package com.vilgodskaia.movieplatformpetproject.api.movieonstreamingplatform.dto
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Getter
-public class MovieOnStreamingPlatformUpdateInputDto {
+public class MovieOnStreamingPlatformCreateDto {
 
+    private UUID movieId;
+    private UUID streamingPlatformId;
     private boolean availableForBuying;
     private boolean availableInSubscription;
     private Integer priceForBuying;
