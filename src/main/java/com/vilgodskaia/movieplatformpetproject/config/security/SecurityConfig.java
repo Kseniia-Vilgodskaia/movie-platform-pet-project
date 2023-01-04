dda295f18901779c9b73dd5eb5d70bc201297747
Kseniia Vilgodskaia <vilgodskaya.ksenia@gmail.com>
2023-01-04 15:35:11 +0200

    Add security for in memory users

---
+++ src/main/java/com/vilgodskaia/movieplatformpetproject/config/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.vilgodskaia.movieplatformpetproject.config.security;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("pesik")
+                .password("pesik")
+                .roles("CLIENT")
+                .build();
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("homa")
+                .password("homa")
+                .roles("ADMIN")
+                .build();
+        UserDetails user3 = User
+                .withDefaultPasswordEncoder()
+                .username("vredina")
+                .password("vredina")
+                .roles("GUEST")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2, user3);
+    }
+
+    @SneakyThrows
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/**").hasAnyRole("ADMIN", "CLIENT")
+                        .anyRequest().authenticated())
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return httpSecurity.build();
+    }
+}
